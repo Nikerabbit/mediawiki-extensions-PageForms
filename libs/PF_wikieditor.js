7aebcf548985fe36336583ff9f744cc3d2931387
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-11-11 18:34:35 +0100

    Hack for wikieditor&pageforms

--- libs/PF_wikieditor.js
+++ libs/PF_wikieditor.js
@@ -13,36 +13,8 @@ window.ext.wikieditor = {
 			if ( mw ) {
 				var input = $( '#' + inputId );
 
-				if ( mw.config.values.wgVersion < "1.33" ) {
-					var toolbarmodules = [ 'jquery.wikiEditor.toolbar', 'jquery.wikiEditor.toolbar.config' ];
-					var dialogmodules = [ 'jquery.wikiEditor.dialogs', 'jquery.wikiEditor.dialogs.config' ];
-				} else {
-					var toolbarmodules = [ 'ext.wikiEditor' ];
-					var dialogmodules = [ 'ext.wikiEditor' ];
-				}
-
-				// load toolbar
-				$.when( mw.loader.using( toolbarmodules ), $.ready ).then( function() {
-					if ( typeof $.wikiEditor.isSupported !== 'function' || $.wikiEditor.isSupported( $.wikiEditor.modules.toolbar ) ) {
-						input.wikiEditor( 'addModule', $.wikiEditor.modules.toolbar.config.getDefaultConfig() );
-
-						// hide sig if required
-						if ( mw.config.get( 'wgWikiEditorEnabledModules' ) && mw.config.get( 'wgWikiEditorEnabledModules.hidesig' ) === true ) {
-							input.wikiEditor( 'removeFromToolbar', {
-								'section': 'main',
-								'group': 'insert',
-								'tool': 'signature'
-							} );
-						}
-					}
-				} );
-
-				// load dialogs
-				$.when( mw.loader.using( dialogmodules ), $.ready ).then( function() {
-					if ( typeof $.wikiEditor.isSupported !== 'function' || $.wikiEditor.isSupported( $.wikiEditor.modules.dialogs ) ) {
-						$.wikiEditor.modules.dialogs.config.replaceIcons( input );
-						input.wikiEditor( 'addModule', $.wikiEditor.modules.dialogs.config.getDefaultConfig() );
-					}
+				mw.loader.using( [ 'ext.wikiEditor' ], function () {
+					mw.addWikiEditor( input );
 				} );
 			}
 		} );
